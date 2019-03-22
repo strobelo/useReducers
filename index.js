680a66a370c47122412accc80ecc2f42599cbdd4
Derek Strobel <dnstrobel000@gmail.com>
2019-03-22 14:32:58 -0600

    added jsdoc string

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { useReducer } from 'react';
-
+/**
+ * combine reducer functionality into one 'store' and one dispatch function
+ * @param  {...function} reducers any number of reducer functions to combine; should have prototype (state, action) => {}
+ */
 export default function useReducers(...reducers) {
     let dispatches = []
     const store = Object.assign(...reducers.map(reducer => {
